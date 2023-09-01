6539edc0781374e704ec1cdb003b7dd7f015556b
KA <xla.2@yandex.ru>
2023-09-01 17:01:35 +0300

    task description, basic imports

---
+++ generation.py
@@ -0,0 +1,20 @@
+import numpy
+import os
+import json
+import random
+
+
+RSEED = 42
+PATH = './'
+
+random.seed(RSEED)
+numpy.random.seed(RSEED)
+# TODO 1: generate random rectangles (non-overlapping, up to 5)
+
+# TODO 2: mark centers, choose framework
+# TODO 3: inscribe 4 shapes into those rectangles, fill-in with colours
+# TODO 4: describe (id,type,x,y,w,h)
+# TODO 5: set up classes
+# TODO 6: serialize via json
+# TODO 7: create dataset (png image <--> json description)
+
