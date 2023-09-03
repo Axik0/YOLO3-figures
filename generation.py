023bcae472127e81f465e5df53c1cbf4a0ec15e7
KA <xla.2@yandex.ru>
2023-09-03 14:11:05 +0300

    figure and rectangle classes, IoU score method, random area split have been implemented

--- generation.py
+++ generation.py
@@ -2,19 +2,139 @@ import numpy
 import os
 import json
 import random
-
+from itertools import accumulate, product
 
 RSEED = 42
 PATH = './'
 
-random.seed(RSEED)
-numpy.random.seed(RSEED)
-# TODO 1: generate random rectangles (non-overlapping, up to 5)
+SIZE = 256
+THR = 25
+PARTS = 4
+MARGIN = 2
 
-# TODO 2: mark centers, choose framework
-# TODO 3: inscribe 4 shapes into those rectangles, fill-in with colours
+
+# random.seed(RSEED)
+# numpy.random.seed(RSEED)
+# TODO 1: generate random axis split (non-overlapping, up to 5)
+def rand_split(n, k: int, l_bound=0):
+    """recursive random split, performs (correlated) random split of n
+     onto k parts, outputs list of k spacers > l_bound that sum up to n"""
+    unc = [random.random() for _ in range(k)]
+    raw_spaces = list(map(lambda x: round(n * x / sum(unc)), unc))
+    m = max(raw_spaces)
+    err = abs(n - sum(raw_spaces))
+    norm, redo, maxes = [], [], []
+    for s in raw_spaces:
+        # put away all elements below threshold (& max element)
+        if s == m:
+            # deal with possibly multiple maxes
+            if not maxes:
+                # adjust one of maxes to get rid of error
+                m_ = m - err
+                err = 0
+            else:
+                m_ = m
+            maxes.append(m_)
+        elif s > l_bound:
+            norm.append(s)
+        else:
+            redo.append(s)
+        # print(norm, maxes, redo)
+    if redo:
+        # add up available space
+        n_redo = sum(redo) + sum(maxes)
+        c_redo = len(redo) + len(maxes)
+        # redistribute remaining space (& max element)
+        if n_redo / c_redo <= l_bound + 5:
+            # prevent recursion overflow for wasted leftovers
+            spaces = rand_split(n, k, l_bound)
+        else:
+            done = rand_split(n_redo, c_redo, l_bound)
+            spaces = norm + done
+        # print(raw_spaces, norm, redo, maxes)
+    else:
+        spaces = norm + maxes
+    assert sum(spaces) <= n, f'Result {spaces} has round-off error(s)'
+    assert len(spaces) == k, f'Result {spaces} has wrong length'
+    return spaces
+
+
+# try:
+#     for i in range(10000):
+#         rand_split(256, 5, THR)
+#     print('10K-pass')
+# except AssertionError:
+#     print('function behaves wrong')
+
+def get_centers(n=SIZE, k=PARTS, l_bound=THR):
+    """convenient function that generates by default """
+    spacers = rand_split(n, k, l_bound)
+    delimiters = [0] + list(accumulate(spacers))[:-1] + [256]
+    centers = [round(p + (f - p) / 2) for f, p in zip(delimiters[1:], delimiters[:-1])]
+    half_sizes = [s / 2 for s in spacers]
+    print(f'We have {len(spacers)} intervals allocated as {delimiters},  their centers are {centers}')
+    return centers, half_sizes
+
+
+# allocate Ox, Oy projections of possible rectangles
+x, y = get_centers(), get_centers()
+# generate product of all xs, ys (all possible combinations without replacement)
+centers_xy, half_sizes_wh = list(product(x[0], y[0])), list(product(x[1], y[1]))
+print(f'{len(centers_xy)} rectangles (and their shapes) in total')
+
+# TODO 2: set up classes
+
+
+class Figure:
+    def __init__(self, center: tuple, half_size: tuple):
+        self.shape = None
+        self.x, self.y = center
+        self.half_w, self.half_h = half_size
+
+
+class Rectangle(Figure):
+    def __init__(self, center, half_size):
+        super().__init__(center, half_size)
+        self.shape = self.__class__.__name__
+        self.wh = [2 * d for d in (self.half_w, self.half_h)]
+        self.area = self.wh[0] * self.wh[1]
+        # alternative minmax vertex representation
+        self.ve_min = round(self.x - self.half_w), round(self.y - self.half_h)
+        self.ve_max = round(self.x + self.half_w), round(self.y + self.half_h)
+
+    def __repr__(self):
+        return f'Figure {self.shape} starts at {self.ve_min} with width {self.wh[0]}, height {self.wh[1]} and area {self.area}'
+
+    def get_aoi(self, other):
+        """calculates (area of) intersection of two rectangls"""
+        assert isinstance(other, Rectangle), f'object {other} is not an instance of {self.__class__.__name__}'
+        # nearest max vertex - farthest min vertex
+        dx = min(self.ve_max[0], other.ve_max[0]) - max(self.ve_min[0], other.ve_min[0])
+        dy = min(self.ve_max[1], other.ve_max[1]) - max(self.ve_min[1], other.ve_min[1])
+        if dx >= 0 and dy >= 0:
+            return dx*dy
+        else:
+            print('no intersection')
+
+    def get_iou(self, other):
+        """produces IoU ratio (percentage)"""
+        assert isinstance(other, Rectangle), f'object {other} is not an instance of {self.__class__.__name__}'
+        aoi = self.get_aoi(other)
+        if aoi:
+            aou = self.area + other.area - aoi
+            iou = round(aoi/aou, 2)*100
+        else:
+            iou = 0
+        return iou
+
+
+# box = Rectangle((30, 20), (5, 7))
+# print(box)
+# box2 = Rectangle((30, 15), (3, 7))
+# print(box2)
+# print(box.get_iou(box2))
+# TODO 3: choose framework
 # TODO 4: describe (id,type,x,y,w,h)
-# TODO 5: set up classes
+# TODO 5: inscribe 4 shapes into those rectangles, fill-in with colours
 # TODO 6: serialize via json
 # TODO 7: create dataset (png image <--> json description)
-
