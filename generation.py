b3ebe062e128ac88808dd2c3a24dbf8658806bf0
KA <xla.2@yandex.ru>
2023-09-14 16:10:10 +0300

    IoU calculation has been (re-)implemented, nothing tested

--- generation.py
+++ generation.py
@@ -553,6 +553,34 @@ id_to_cname = [c((100, 100), (50, 50)).shape for c in id_to_class]
 cname_to_id = {cn: i for i, cn in enumerate(id_to_cname)}
 
 
+def vertex_repr(bbox):
+    """converts from (x_center, y_center, w, h) to minimal-maximal vertex representation"""
+    xc, yc, w, h = bbox
+    vertex_min, vertex_max = (xc - w/2, yc - h/2), (xc + w/2, yc + h/2)
+    return vertex_min, vertex_max
+
+
+def iou(base_box, list_of_boxes):
+    """treats bbox as (x_center, y_center, w, h), most likely normalized to [0..1],
+    calculates areas of intersection and union for each box in the list, outputs list of IoU scores in [0..1]"""
+    ious = []
+    # represent boxes with their minmax vertices + calculate an area in a single pass
+    vr = list(map(lambda bb: (*vertex_repr(bb), bb[2] * bb[3]), list_of_boxes + [base_box]))
+    vmi_0, vma_0, area_0 = vr.pop()
+    for b in list_of_boxes:
+        vmi_b, vma_b, area_b = b
+        # get aoi, nearest max vertex - farthest min vertex
+        dx = min(vma_0[0], vma_b[0]) - max(vmi_0[0], vmi_b[0])
+        dy = min(vma_0[1], vma_b[1]) - max(vmi_0[1], vmi_b[1])
+        aoi_0b = dx * dy if dx >= 0 and dy >= 0 else 0  # 0 means no intersection
+        # get aou ~ sum of areas -- aoi (not to count twice)
+        aou_0b = area_0 + area_b - aoi_0b
+        assert aou_0b != 0, 'some of boxes have 0 width or height, incorrect input'
+        iou_0b = aoi_0b/aou_0b
+        ious.append(iou_0b)
+    return ious
+
+
 class FiguresDataset(VisionDataset):
     def __init__(self, transforms, root=PATH, iou_threshold=0.5, anchors=ANCHORS, gs=GRID_SIZES):
         super().__init__(root)
@@ -571,7 +599,7 @@ class FiguresDataset(VisionDataset):
         # establish 1:1 correspondence (at each scale): ground truth bounding box <-> anchor box and cell as target
         # let's keep track of bboxes that have never been mapped (on all 3 scales) for further investigation purposes
         self.unused_bboxes = []
-        self.targets = [self.build_targets(bb_list) for bb_list in self.bboxes]  # it's 1:1 within an image only
+        self.targets = [self.build_targets(bb_list) for bb_list in self.bboxes]  # it's 1:1 within an image only!!!
         assert len(self.targets) == len(self.bboxes), 'wrong targets, check their builder method'
 
     def __getitem__(self, i):
@@ -600,8 +628,10 @@ class FiguresDataset(VisionDataset):
                 # cell choice - put current s-grid onto original image, take a cell w/ bb center inside (if not taken)
                 cx, cy = int(gs * x), int(gs * y)  # cell ~ top left corner relative (to grid) coordinates
                 # anchor choice - 'best' (by IoU) of all free anchors at each step, suppress the rest w/ same role (NMS)
-
-                for ai in an_top: #an_top = [2, 1, 0]
+                anchor_ious = iou(base_box=bb, list_of_boxes=al)
+                # sort in descending manner and get sorting indices
+                an_top = sorted(range(len(anchor_ious)), reverse=True, key=lambda _: anchor_ious[_])
+                for ai in an_top:  # an_top = [2, 1, 0]
                     # check if the anchor at this cell is already used (=mapped to another bbox)
                     used = td[ai, cx, cy, 0]
                     if not used:
@@ -614,7 +644,7 @@ class FiguresDataset(VisionDataset):
                             td[ai, cx, cy, 6] = ci  # attach class id label
                             found = True
                         # NMS -- taken best, suppress rest (if free and detect this bbox quite good)
-                        elif found and iou(ai) > self.iou_thr:
+                        elif found and anchor_ious[ai] > self.iou_thr:
                             # best anchor is assigned, get rid of rest (only the ones that also detect this bbox ok!)
                             td[ai, cx, cy, 0] = -1  # ignore, i.e. they're NOT available for future (other bboxes)
                 # replace target_dummy with td (even if td hasn't changed, i.e. all zeros)
@@ -624,7 +654,6 @@ class FiguresDataset(VisionDataset):
             if not found:  # found=None means that for-cycle has never started, that's impossible but nevertheless
                 self.unused_bboxes.append(bb)
         return targets
-
 
 
 if __name__ == '__main__':
