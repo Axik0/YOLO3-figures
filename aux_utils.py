error has been fixed: inaccurate assignment within raw_transform lead to cumulative overflow of prediction tensor => incorrect loss calculation

aux_utils.py
@@ -79,10 +79,11 @@ def raw_transform(raw_net_out_s, anchors_s, partial=False):
     """this function is intended to be applied scalewise, partial=True limits this transformation to 1:3 ix;
     according to yolo design, this transforms (batched) raw NN output (just 4 coords) to the desired (target) format"""
     # anchors = anchors_s.reshape(1, 3, 1, 1, 2)  # current anchor ~ 3*2 tensor, add dimensions to multiply freely
-    raw_net_out_s[..., 1:3] = torch.sigmoid(raw_net_out_s[..., 1:3])
+    res = raw_net_out_s.clone()     # otherwise mutates input tensors outer scope
+    res[..., 1:3] = torch.sigmoid(raw_net_out_s[..., 1:3])
     if not partial:
-        raw_net_out_s[..., 3:5] = torch.exp(raw_net_out_s[..., 3:5]) * anchors_s
-    return raw_net_out_s
+        res[..., 3:5] = torch.exp(raw_net_out_s[..., 3:5]) * anchors_s
+    return res
 
 
 def tl_to_bboxes(tar_like: list, gs, anchors, raw=False):
@@ -162,21 +163,24 @@ def batch_sample(batch, model, loss_fn=None, size=10, **pick_kwargs):
         size translates to matplotlib figure size"""
     imgs, tars = batch
     bs = imgs.shape[0]
-    preds = model(imgs)
+    model.eval()
+    with torch.inference_mode():
+        preds = model(imgs)
 
-    # draw image with target bboxes, then predictions (on top) for all elements of batch
-    sample_list = [pick((pick((imgs[i, ...], tuple(ts[i, ...] for ts in tars)), **pick_kwargs),
-                         tuple(ps[i, ...] for ps in preds)), raw=True, **pick_kwargs)
-                   for i in range(bs)]
-    # lay out list of processed images (tensors) with boxes
-    plt.figure(figsize=(size, size))
-    show_img(torchvision.utils.make_grid(sample_list, nrow=np.sqrt(bs).astype(int)))
-    loss_info_str = ''
-    if loss_fn:
-        loss_data = [(loss_fn(pred_s=preds[s], tar_s=tars[s], scale=s), loss_fn.get_state()) for s in range(3)]
-        loss, state = tuple(map(lambda tl: torch.sum(torch.stack(tl, dim=0), dim=0), zip(*loss_data)))
-        loss_info_str = f' with loss {tuple(map(lambda x: round(x, 2), state.tolist()))}'
-    plt.title(f'Detections on a batch of {bs} images' + loss_info_str)
+        # draw image with target bboxes, then predictions (on top) for all elements of batch
+        sample_list = [pick((pick((imgs[i, ...], tuple(ts[i, ...] for ts in tars)), **pick_kwargs),
+                             tuple(ps[i, ...] for ps in preds)), raw=True, **pick_kwargs)
+                       for i in range(bs)]
+        # lay out list of processed images (tensors) with boxes
+        plt.figure(figsize=(size, size))
+        show_img(torchvision.utils.make_grid(sample_list, nrow=np.sqrt(bs).astype(int)))
+        if loss_fn:
+            loss_data = [(loss_fn(pred_s=preds[s], tar_s=tars[s], scale=s), loss_fn.get_state()) for s in range(3)]
+            loss, state = tuple(map(lambda tl: torch.sum(torch.stack(tl, dim=0), dim=0), zip(*loss_data)))
+            loss_info_str = f' with loss {tuple(map(lambda x: round(x, 2), state.tolist()))}'
+        else:
+            loss_info_str = ''
+        plt.title(f'Detections on a batch of {bs} images' + loss_info_str)
 
 
 class KIoU(KMeans):

modules.py
@@ -44,7 +44,8 @@ class FiguresDataset(Dataset):
     part=(start_id, end_id) translates to slicing on loaded data
     upd_stats allows to get actual mean and standard deviation values per img channel, very slow"""
 
-    def __init__(self, aug=(), part=slice(None, None), iou_threshold=0.5, anchors=ANCHORS, gs=GRID_SIZES, upd_stats=False):
+    def __init__(self, aug=(), part=slice(None, None), iou_threshold=0.5, anchors=ANCHORS, gs=GRID_SIZES,
+                 upd_stats=False):
         super().__init__()
         self.iou_thr = iou_threshold
         # anchors are set by just (relative) width & height, nested tuple 3*3
@@ -152,29 +153,30 @@ class YOLOLoss(nn.Module):
 
         # current anchor_s ~ 3*2 tensor, add dimensions to multiply freely
         anchors_s = torch.tensor(ANCHORS[scale]).reshape(1, 3, 1, 1, 2)
-        # transform predictions to targets using given anchors
+        # transform predictions to targets using given anchors, clone to avoid changes of an original tensor
         pred_st = raw_transform(pred_s, anchors_s)
 
         # has object loss: let object presence probability = iou_score, learns to predict not just 1 but own iou with gt
         # take the ones with object and compare with target bbox by iou
-        ious = iou_pairwise(pred_st[..., 1:5][yobj], tar_s[..., 1:5][yobj])#.detach()  # yet unsure about this detachment
-        self.yo_loss = self.bce(pred_st[..., 0:1][yobj], ious * tar_s[..., 0:1][yobj])
+        ious = iou_pairwise(pred_st[..., 1:5][yobj], tar_s[..., 1:5][yobj]).detach()  # yet unsure about this detachment
+        yo_loss = self.bce(pred_st[..., 0:1][yobj], ious * tar_s[..., 0:1][yobj])
 
         # bounding box loss: let's transform (part of) target to predictions, this trick allows better gradient flow,
         pred_st_part = torch.cat([pred_st[..., 1:3], pred_s[..., 3:5]], dim=-1)  # part is same sigmoid as before
-        tar_s[..., 3:5] = torch.log(EPS ** 3 + tar_s[..., 3:5]) / anchors_s  # inverse transform part of target
-        self.bo_loss = self.mse(pred_st_part[..., 0:4][yobj], tar_s[..., 1:5][yobj])
+        tar_st_part = torch.cat([tar_s[..., 1:3], torch.log(EPS ** 3 + tar_s[..., 3:5]) / anchors_s],
+                                dim=-1)  # inverse transform part of target
+        bo_loss = self.mse(pred_st_part[yobj], tar_st_part[yobj])
 
         # no object loss: 0:1 is a trick to keep dimensions and don't throw an error by mask
-        self.no_loss = self.bce(pred_s[..., 0:1][nobj], tar_s[..., 0:1][nobj])
+        no_loss = self.bce(pred_s[..., 0:1][nobj], tar_s[..., 0:1][nobj])
         # class loss: takes C logits, outputs single number, compared w/ target class
-        self.ca_loss = self.ent(pred_s[..., 5:][yobj], tar_s[..., 5][yobj].long())
+        ca_loss = self.ent(pred_s[..., 5:][yobj], tar_s[..., 5][yobj].long())
 
         # stack 4 separate current loss values into 4-tensor
-        self.combo_loss = torch.stack((self.la_abs * self.no_loss,
-                     self.la_prs * self.yo_loss,
-                     self.la_box * self.bo_loss,
-                     self.la_cls * self.ca_loss))
+        self.combo_loss = torch.stack((self.la_abs * no_loss,
+                                       self.la_prs * yo_loss,
+                                       self.la_box * bo_loss,
+                                       self.la_cls * ca_loss))
 
         return torch.sum(self.combo_loss)
 

training.py
@@ -53,7 +53,7 @@ def load_ch(model, optimizer, path=os.path.join(CFP, CH_NAME)):
         print(f'no checkpoint there {path}')
 
 
-def run(model, dataloader, loss_fn, amp_scaler, optimizer=None, device=DEVICE, agg=True):
+def run(model, dataloader, loss_fn, amp_scaler=None, optimizer=None, device=DEVICE, agg=True):
     """universal (forward + optional backward) pass, requires tqdm-wrapped dataloader
     loss component mean aggregation (over batch dimension) is set up as a default output,
     """
@@ -89,7 +89,7 @@ def run(model, dataloader, loss_fn, amp_scaler, optimizer=None, device=DEVICE, a
 
 
 def train(model, dataloader_train, loss_fn, optimizer, n_epochs,
-          amp_scaler=None, device=DEVICE, dataloader_test=None, ep=2, load=False, palette=PALETTE):
+          amp_scaler, device=DEVICE, dataloader_test=None, ep=2, load=False, palette=PALETTE):
     """Model training procedure w/ possible evaluation (if test data is provided) and autosave/autoload
         ep -- model evaluation period, any integer >= 0,
         amp_scaler should be an instance of torch.cuda.amp.GradScaler for automated mixed precision training on GPU"""